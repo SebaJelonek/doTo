0d5a6f0895e4afd10064c22b625bfa43a3c38eea
Sebastian Jelonek <jeloneksebastian93@gmail.com>
2025-06-03 08:56:29 +0200

    BACKEND - added function to generate jwt

--- go-back/internals/handlers/user/login.go
+++ go-back/internals/handlers/user/login.go
@@ -1,11 +1,16 @@
 package users
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
 	"database/sql"
+	"encoding/base64"
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 	"strings"
+	"time"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -21,8 +26,19 @@ type User struct {
 	Password string
 }
 
+type Header struct {
+	Alg  string `json:"alg"`
+	Type string `json:"typ"`
+}
+
+type Payload struct {
+	UserID     int   `json:"userId"`
+	Expiration int64 `json:"exp"`
+}
+
 func LoginUser(dbConnection *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+
 		var userLogin UserLogin
 		var user User
 		err := json.NewDecoder(r.Body).Decode(&userLogin)
@@ -48,6 +64,18 @@ func LoginUser(dbConnection *sql.DB) http.HandlerFunc {
 		isCorrect := CheckPassword(userLogin.Password, user.Password)
 
 		if isCorrect {
+			header := Header{
+				Alg:  "HS256",
+				Type: "JWT",
+			}
+			payload := Payload{
+				UserID:     user.ID,
+				Expiration: time.Now().UnixMilli() + 60000,
+			}
+
+			jwt := GenerateJWT(header, payload)
+			log.Println(jwt)
+
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(200)
 			json.NewEncoder(w).Encode(map[string]any{
@@ -67,4 +95,34 @@ func LoginUser(dbConnection *sql.DB) http.HandlerFunc {
 func CheckPassword(password string, hashedPassword string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 	return err == nil
+}
+
+func GenerateJWT(header Header, payload Payload) string {
+	encoder := base64.URLEncoding.WithPadding(base64.NoPadding)
+	secret := os.Getenv("SECRET")
+
+	jsonHeader, err := json.Marshal(header)
+	if err != nil {
+		log.Println(err)
+		panic(err)
+	}
+
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		log.Println(err)
+		panic(err)
+	}
+
+	jsonWebHeader := encoder.EncodeToString(jsonHeader)
+	jsonWebPayload := encoder.EncodeToString(jsonPayload)
+
+	jw := jsonWebHeader + "." + jsonWebPayload
+
+	hmac := hmac.New(sha256.New, []byte(secret))
+	hmac.Write([]byte(jw))
+
+	jwHashed := hmac.Sum(nil)
+
+	jwt := jw + "." + encoder.EncodeToString(jwHashed)
+	return jwt
 }
